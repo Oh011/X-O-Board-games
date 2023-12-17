799b1e1a532e79f5c66f2a944fb72089f1dec330
Mohamed Aber <63326551+themohamedaber@users.noreply.github.com>
2023-12-17 15:31:06 +0200

    Update X_O_Board.cpp

--- X_O_Board.cpp
+++ X_O_Board.cpp
@@ -1,13 +1,1 @@
-Four_in_a_row_Board::Four_in_a_row_Board() {
 
-    n_rows=6;
-    n_cols=7;
-
-    board = new char*[n_rows];
-    for (int i = 0; i < n_rows; i++) {
-        board [i] = new char[n_cols];
-        for (int j = 0; j < n_cols; j++)
-            board[i][j] ='0';
-    }
-
-}
