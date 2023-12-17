cb81d297c733c95f233a46098fac7006ed7e99aa
omar hisham <145376235+Oh011@users.noreply.github.com>
2023-12-17 19:06:54 +0200

    Update Pyramid_Tic_Tac_Toe_Board.cpp

--- Pyramid_Tic_Tac_Toe_Board.cpp
+++ Pyramid_Tic_Tac_Toe_Board.cpp
@@ -53,3 +53,68 @@ bool Pyramid_Tic_Tac_Toe_Board::update_board(int x, int y, char mark) {
         n_moves++;
         return true;
     }
+
+}
+
+
+
+// Check if there is a winner on the Pyramid Tic-Tac-Toe board
+bool Pyramid_Tic_Tac_Toe_Board::is_winner() {
+
+
+
+    // Check for column win and loop through column 2
+    int col_win = 1;
+    for (int i = 0; i <= 1; ++i) {
+        if (board[i][2] == board[i + 1][2] && (board[i][2] != '0'))
+            ++col_win;
+    }
+
+// if we get three consecutive equal marks in column 2 then it is considered a win.
+    if (col_win == 3)
+        return true;
+
+    // Check for row win and  loop through row 1
+    int row_win = 1;
+    for (int i = 1; i <= 2; ++i) {
+        if (board[1][i] == board[1][i + 1] && (board[1][i] != '0'))
+            ++row_win;
+    }
+
+    // if we get three consecutive equal marks in row 1 then it is considered a win.
+    if (row_win == 3)
+        return true;
+
+
+    row_win = 1;
+
+    // Check for row win and  loop through row 2
+    for (int i = 0; i <= 3; ++i) {
+
+        if (row_win == 3)
+            return true;
+
+        else if (board[2][i] == board[2][i + 1] && board[2][i] != '0')
+            ++row_win;
+        else
+            row_win = 1;
+    }
+
+    // if we get three consecutive equal marks in row 2 then it is considered a win.
+    if (row_win == 3)
+        return true;
+
+    // Check for diagonal wins
+
+
+    // Check for diagonal wins (top-right to bottom-left)
+    if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && board[0][2] != '0')
+        return true;
+
+        // Check for diagonal wins (top-left to bottom-right)
+    else if ((board[0][2] == board[1][3]) && (board[1][3] == board[2][4]) && board[0][2] != '0')
+        return true;
+
+    return false;
+}
+
