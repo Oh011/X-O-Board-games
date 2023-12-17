48da6421fd374f3eb6bd6e2db6f781c04b40d10d
Mohamed Aber <63326551+themohamedaber@users.noreply.github.com>
2023-12-17 15:33:00 +0200

    Update Four_in_a_row_Board.cpp

--- Four_in_a_row_Board.cpp
+++ Four_in_a_row_Board.cpp
@@ -11,3 +11,24 @@ Four_in_a_row_Board::Four_in_a_row_Board() {
     }
 
 }
+bool Four_in_a_row_Board::update_board(int x, int y, char mark) {
+    // Check if the given coordinates are within the valid range
+    if ((x > 5 || y > 6) || (x < 0 || y < 0))
+        return false;
+
+    // Check if the space below is empty for a valid move
+    if (x < 5 && board[x + 1][y] == '0')
+        return false;
+
+    // Check if the current position is already marked
+    if (board[x][y] != '0') {
+        cout << "This place has a mark\n";
+        return false;
+    }
+    else {
+        // Update the board with the player's mark
+        board[x][y] = toupper(mark);
+        n_moves++;
+        return true;
+    }
+}
